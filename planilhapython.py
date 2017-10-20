f6d312378fede5250a3406fb930a24687460da2b
edilson.borges <edilson.borges@agrodefesa.go.gov.br>
2017-10-19 23:49:21 -0200

    pprint spreadsheet

--- planilhapython.py
+++ planilhapython.py
@@ -2,11 +2,14 @@ import gspread
 
 from oauth2client.service_account import ServiceAccountCredentials
 
+import pprint
+
 scope = ['https://spreadsheets.google.com/feeds']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
 sheet = client.open('planilha_python').sheet1
 
+pp = pprint.PrettyPrinter()
 planilha_python = sheet.get_all_records()
-print(planilha_python)
+pp.pprint(planilha_python)
