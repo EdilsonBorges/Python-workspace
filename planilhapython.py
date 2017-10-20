d6275b2f9794666f4be6c2bcdba33d54399d9b8a
edilson.borges <edilson.borges@agrodefesa.go.gov.br>
2017-10-20 09:11:15 -0200

    bug fix for update

--- planilhapython.py
+++ planilhapython.py
@@ -14,10 +14,10 @@ pp = pprint.PrettyPrinter()
 
 #result = sheet.get_all_records()	# all records
 #result = sheet.row_values(10)		# just a row
-result = sheet.cell(5,13).value		# specific cell
+result = sheet.cell(5,4).value		# specific cell
 
 pp.pprint(result)
 
-sheet.update_cell(5,13,'testing update') # updating record
-result = sheet.cell(5,13).value
-pp.print(result)
+sheet.update_cell(5,4,'testing update') # updating record
+result = sheet.cell(5,4).value
+pp.pprint(result)
