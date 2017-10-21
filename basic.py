09210dcbca0af5dff014eb3b9e185c1c66370168
Edilson Borges <edilsonfb@gmail.com>
2017-10-21 08:48:16 -0200

    comments types, single, double and triple quotes for long strings

--- basic.py
+++ basic.py
@@ -1,1 +1,18 @@
-print('Hi')
+# Comments
+
+# This is a single line comment
+
+'''
+
+This is multi line comment
+
+
+'''
+
+print('Hi') # doesn't matter ' or "
+
+
+print("""Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?""") # triple quotes
+
+
+
