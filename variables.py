fbf6be5edd9463cda4ab11ffc4810d6b4e69f27b
Edilson Borges <edilsonfb@gmail.com>
2017-10-21 09:03:26 -0200

    data type, string, int, float, list, dictionary

--- variables.py
+++ variables.py
@@ -1,3 +1,16 @@
 greeting = 'Hello World'
 greeting = 'Hello Everyone' # reassign
 print(greeting)
+
+# Data type
+myStr = 'Hello'
+myInt = 25
+myFloat = 1.3
+myList = [1,2,3,'testing']
+myDict = {'a': 1, 'b': 2, 'c':3}
+
+print(type(myStr), myStr)
+print(type(myInt), myInt)
+print(type(myFloat), myFloat)
+print(type(myList), myList)
+print(type(myDict), myDict)
