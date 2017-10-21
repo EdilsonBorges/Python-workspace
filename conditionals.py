00445cffeb820e2f5435b88c56062b3fab45a0bc
Edilson Borges <edilsonfb@gmail.com>
2017-10-21 09:13:06 -0200

    else and elif

--- conditionals.py
+++ conditionals.py
@@ -1,4 +1,18 @@
-x = 4
+x = 7
 
 if x < 6:
 	print('This is tue')
+
+if x < 6:
+	print('This is true')
+else:
+	print('This is false')
+
+color = 'yellow'
+
+if color == 'red':
+	print('color is red')
+elif color == 'blue':
+	print('color is blue')
+else:
+	print('color is not red and blue')
