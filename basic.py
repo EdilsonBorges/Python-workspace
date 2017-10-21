e9ce93aeb5687a7dc0be218b1a2e68d6368b3c64
Edilson Borges <edilsonfb@gmail.com>
2017-10-21 08:53:58 -0200

    printing single or range characters

--- basic.py
+++ basic.py
@@ -14,5 +14,9 @@ print('Hi') # doesn't matter ' or "
 
 print("""Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?""") # triple quotes
 
+# print substring
+print('Hi there'[4]) # single char
+print('Hi there'[4:6]) # range
 
+print('Hi\nthere\n!')
 
