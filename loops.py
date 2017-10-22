acd452e5e2d2d1ed4e6d322997d38328000360ec
Edilson Borges <edilsonfb@gmail.com>
2017-10-22 12:14:06 -0200

    iterating by seq index

--- loops.py
+++ loops.py
@@ -2,3 +2,9 @@ people = ['John', 'Sarah', 'Tim', 'Bob']
 
 for person in people:
 	print('Current person: ', person)
+
+print()
+
+# iterate by seq index
+for i in range(len(people)):
+	print('Current person: ', people[i])
