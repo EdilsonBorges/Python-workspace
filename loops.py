1624158e406b37990185a9041fd1d64114124d42
Edilson Borges <edilsonfb@gmail.com>
2017-10-22 12:18:43 -0200

    for loop with range and while loop

--- loops.py
+++ loops.py
@@ -8,3 +8,14 @@ print()
 # iterate by seq index
 for i in range(len(people)):
 	print('Current person: ', people[i])
+
+for i in range(0, 10):
+	print(i)
+
+# while loop
+count = 0
+while count < 10:
+	print('Count: ', count)
+	count += 1
+	if count == 5:
+		break
