663b061f820bcdff025bd9c3fbcd94c7528f25e3
Edilson Borges <edilsonfb@gmail.com>
2017-10-23 20:29:11 -0200

    new class Customer using Person class

--- classes.py
+++ classes.py
@@ -29,3 +29,27 @@ brad = Person('Brad', 'brad@gmail.com')
 # brad.set_email('brad@gmail.com')
 # print(brad.get_name(), '-', brad.get_email())
 print(brad.toString())
+
+class Customer(Person):
+	__balance = 0
+
+	def __init__(self, name, email, balance):
+		self.__name = name
+		self.__email = email
+		self.__balance = balance
+		super(Customer, self).__init__(name, email)
+
+	def set_balance(self, balance):
+		self.__balance = balance
+		
+	def get_balance(self):
+		return self.__balance
+
+	def toString(self):
+		return '{} has a balance of {} and can be contacted at {}'.format(self.__name, self.__balance, self.__email)
+
+john = Customer('John', 'john@gmail', 100)
+print(john.toString())
+
+kate = Customer('Kate Smith', 'kate@yahoo.com', 5000)
+print(kate.toString())
