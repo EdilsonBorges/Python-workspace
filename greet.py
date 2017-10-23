59f1a2e479beab747e9f5afc4d4ec8e29db9fca5
Edilson Borges <edilsonfb@gmail.com>
2017-10-23 19:57:42 -0200

    modules: importing complete object and just a method

---
+++ greet.py
@@ -0,0 +1,6 @@
+def sayHello(name):
+	print ('Hello', name, 'From the greet module')
+	return
+
+def sayGoodbye(name):
+	print ('Bye bye', name)
---
+++ modules.py
@@ -0,0 +1,8 @@
+import greet					# importing all object greet
+
+greet.sayHello('John')
+
+
+from greet import sayGoodbye	# importing just the function from greet
+
+sayGoodbye('John')
