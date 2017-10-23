ae006e6deb3cb200bcece68b13791c0167d4e2d4
Edilson Borges <edilsonfb@gmail.com>
2017-10-23 19:52:59 -0200

    string functions

---
+++ stringfunctions.py
@@ -0,0 +1,28 @@
+myStr = 'Hello There World!'
+
+print(myStr.capitalize())
+
+print(myStr.swapcase())
+
+print(len(myStr))
+
+print(myStr.replace('World', 'Everyone'))
+
+sub = 'l'
+print(myStr.count(sub)) # counting string occurrences
+
+print(myStr.startswith('Hello'))
+
+print(myStr.endswith('!'))
+
+print(myStr.split())
+
+print(myStr.find('World'))
+
+print(myStr.index('d')) # error if there's no result
+
+print(myStr.isalnum())
+
+print(myStr.isalpha())
+
+print(myStr.isnumeric())
