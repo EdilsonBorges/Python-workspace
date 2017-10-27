6ef9298e76cd7e1178ec051a6e0143e88346c6c9
Edilson Borges <edilsonfb@gmail.com>
2017-10-27 19:23:50 -0200

    adding new todos

--- apps/todolist/todos/views.py
+++ apps/todolist/todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import Todo
@@ -19,7 +19,11 @@ def details(request, id):
 
 def add(request):
 	if(request.method == 'POST'):
-		return print('teste')
+		title = request.POST['title']
+		text = request.POST['text']
+		todo = Todo(title=title, text=text)
+		todo.save()
+		return redirect('/todos')
 	else:
 		return render(request, 'add.html')
 
